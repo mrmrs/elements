afc57b9562d821c0f43cf538edfedcb68568cce5
mrmrs <hi@mrmrs.cc>
2018-07-11 11:35:37 +0100

    Add select element

---
+++ src/elements/Select.js
@@ -0,0 +1,12 @@
+import glamorous from 'glamorous'
+import {space, display, width} from 'styled-system'
+
+const Select = glamorous.Select(space, display, width, {
+
+})
+
+Select.defaultProps = {
+
+}
+
+export default Select
