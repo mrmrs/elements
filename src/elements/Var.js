4073bef92c30dee4d5f6cd028ad222dc28aef7c0
mrmrs <hi@mrmrs.cc>
2018-07-27 14:55:37 +0100

    Add fallback if nothing is declared in the theme

--- src/elements/Var.js
+++ src/elements/Var.js
@@ -1,9 +1,12 @@
 import glamorous from 'glamorous'
-import {space, width, fontSize, color} from 'styled-system'
+import {space, width, display, fontSize, fontWeight, color} from 'styled-system'
+
 import theme from '../theme'
 
-const Var = glamorous.var(space, width, fontSize, color, {
-  fontFamily: theme.fontFamily.mono,
+const Var = glamorous.var(space, width, display, fontSize, fontWeight, color, {
+  fontFamily: theme.fontFamily.mono
+    ? theme.fontFamily.mono
+    : 'monospace, monospace',
 })
 
 Var.defaultProps = {}
