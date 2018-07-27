fa3d58578de2e2585a4628d6f38245d6827bca99
mrmrs <hi@mrmrs.cc>
2018-07-27 15:23:46 +0100

    Add flex props to Header

--- src/elements/Header.js
+++ src/elements/Header.js
@@ -1,10 +1,21 @@
 import glamorous from 'glamorous'
-import {space, width, display, fontSize, textAlign, color} from 'styled-system'
+import {
+  space,
+  width,
+  display,
+  alignItems,
+  justifyContent,
+  fontSize,
+  textAlign,
+  color,
+} from 'styled-system'
 
 const Header = glamorous.header(
   space,
   width,
   display,
+  alignItems,
+  justifyContent,
   fontSize,
   textAlign,
   color,
