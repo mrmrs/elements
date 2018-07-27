d9625ca167fb42ebd458a77e78734b3b03b08f9f
mrmrs <hi@mrmrs.cc>
2018-07-27 15:54:02 +0100

    Add some border props to the span element

--- src/elements/Span.js
+++ src/elements/Span.js
@@ -2,21 +2,29 @@ import glamorous from 'glamorous'
 import {
   space,
   width,
+  display,
   fontSize,
   fontWeight,
   lineHeight,
   textAlign,
   color,
+  borders,
+  borderColor,
+  borderWidth,
 } from 'styled-system'
 
 const Span = glamorous.span(
   space,
   width,
+  display,
   fontSize,
   fontWeight,
   lineHeight,
   textAlign,
   color,
+  borders,
+  borderColor,
+  borderWidth,
   {},
 )
 
