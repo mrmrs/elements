2f12c0e94d879a23eb39e1524fd9c55ecf939c24
mrmrs <hi@mrmrs.cc>
2018-07-27 14:59:25 +0100

    Reformatting with Prettier

--- src/elements/Tbody.js
+++ src/elements/Tbody.js
@@ -1,9 +1,7 @@
 import glamorous from 'glamorous'
 import {space, width, fontSize, color} from 'styled-system'
 
-const Tbody = glamorous.tbody(space, width, fontSize, color,  {
-
-})
+const Tbody = glamorous.tbody(space, width, fontSize, color, {})
 
 Tbody.defaultProps = {
   w: 1,
